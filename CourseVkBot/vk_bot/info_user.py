b970ac3602c0f03dc417b680490cc905fdd79583
yakummi <GorobtsovIvan@mail.ru>
2022-10-01 16:19:37 +0300

    add idea for test

--- CourseVkBot/vk_bot/info_user.py
+++ CourseVkBot/vk_bot/info_user.py
@@ -2,17 +2,18 @@ from bot_configs.token import TOKEN
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 from vk_api.keyboard import VkKeyboard, VkKeyboardColor
-
+import datetime
 
 session = vk_api.VkApi(token=TOKEN)
 
 vk = session.get_api()
 
 # переделал для кнопок
-def send_message(user_id, message, keyboard=None):
+def send_message(user_id, message, keyboard=None, photo=None):
     post = {'user_id': user_id,
             'message': message,
-            'random_id': 0}
+            'random_id': 0,
+            'attachment':photo}
 
     if keyboard != None:
         post['keyboard'] = keyboard.get_keyboard()
@@ -21,21 +22,36 @@ def send_message(user_id, message, keyboard=None):
 
     session.method('messages.send', post)
 
+def search_people(city, sex): # доработать функция стадия test
+    search = session.method('users.search', {'sort': 0, # sort - 0 по популярности
+                                             'hometown': city,
+                                             'sex': sex})
+    print(search)
+
+
 def get_info_user(user_id):
+    info = {}
     user_info = session.method('users.get', {'user_id': user_id,
                                              'fields': 'sex, bdate'}) # 1 - жен; 2 - муж; 0 - не указан
     print(user_info)
     for information in user_info:
         id_user = information['id'] # инфа
         bdate = information['bdate']
+        year_now = datetime.datetime.now().year
+        bdate = int(year_now) - int(bdate.split('.')[2]) # получил возраст пользователя
         sex = information['sex']
         name = information['first_name']
         surname = information['last_name']
+        info[id_user] = {'age': bdate,
+                         'sex': sex,
+                         'name': name,
+                         'surname': surname}
 
 # доработка - получить последнее сообщение пользователя
 def get_message(user_id):
     text = session.method('messages.getHistory', {'user_id': user_id,
                                            'count': 1})
+
     print(text)
 
 for event in VkLongPoll(session).listen():
@@ -44,7 +60,7 @@ for event in VkLongPoll(session).listen():
         user_id = event.user_id
 
         if text == '/start':
-            send_message(user_id, message='Привет! Я бот!')
+            send_message(user_id, message='Привет, я Бот, который найдет тебе нового друга в твоем городе!\nСледуй инструкциям, чтобы я смог понять тебя и твои пожелания!', photo=r'photo-216252230_457239018')
             get_info_user(user_id)
 
         # стадия теста
@@ -63,10 +79,13 @@ for event in VkLongPoll(session).listen():
 
             send_message(user_id, 'Чтобы начать поиск, нам необходимо собрать с вас некую информацию. Введите ваш город.', keyboard)
 
-        if text == "указать город":
-            send_message(user_id, 'Введите название вашего города.')
-            text = text
-            get_message(user_id)
-            # доработать "получить сообщение пользователя на кнопку"
+            if text == "указать город":
+                send_message(user_id, 'Введите название вашего города.')
+                text = text
+                get_message(user_id)
+                # доработать "получить сообщение пользователя на кнопку"
 
-
+            # проверка на наличие города у пользователя в database
+                if text == 'начать поиск':
+                    pass
+                    # пользуемся https://dev.vk.com/method/users.search
