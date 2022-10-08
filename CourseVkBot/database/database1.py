adc0b5693db08ae93967bce693b2ca57de59f330
Vladimir Kovalev <wault@mail.ru>
2022-10-08 22:57:08 +0500

    database v1.3

--- CourseVkBot/database/database1.py
+++ CourseVkBot/database/database1.py
@@ -1,11 +1,14 @@
 import psycopg2
 from dataclasses import dataclass
 from config import Settings
+
 import vk_api
 from bot_configs.token_user_vk import TOKEN_VK_USER
+session = vk_api.VkApi(token=TOKEN_VK_USER)
+
 conn = psycopg2.connect(database=Settings.DATABASE, user=Settings.USER, password=Settings.PASSWORD)
 
-session = vk_api.VkApi(token=TOKEN_VK_USER)
+
 
 
 @dataclass
@@ -20,7 +23,8 @@ class Database:
         with conn.cursor() as cur:
             cur.execute(f'''
                         CREATE TABLE IF NOT EXISTS {self.tables_names['selection']}(
-                            id_selection INT not null generated always as identity primary key,
+                            id_selection SERIAL PRIMARY KEY,
+                            requester INT,
                             age INT,
                             gender VARCHAR(10),
                             city VARCHAR(100),
@@ -30,7 +34,7 @@ class Database:
             # ==================================================
             cur.execute(f'''
                         CREATE TABLE IF NOT EXISTS {self.tables_names['preferences']}(
-                            id_preferences INT not null generated always as identity primary key,
+                            id_preferences SERIAL PRIMARY KEY,
                             favorites BOOLEAN,
                             black_list BOOLEAN,
                             viewed BOOLEAN
@@ -50,26 +54,118 @@ class Database:
             # ====================================================
             cur.execute(f'''
                        CREATE TABLE IF NOT EXISTS {self.tables_names['photos']}(
-                           id_photo INT,
+                           id_photo SERIAL PRIMARY KEY,
                            id_vk_user INT references {self.tables_names['users']} (id_vk_user) on delete set null,
                            link TEXT,
                            likes BOOLEAN
                        );
                         ''')
             conn.commit()
-        conn.close()
+        # conn.close()
 
     def drop_tables(self):
         with conn.cursor() as cur:
             cur.execute(f'''
-            DROP TABLE {self.tables_names['users']} CASCADE;
-            DROP TABLE {self.tables_names['selection']} CASCADE;
-            # DROP TABLE {self.tables_names['preferences']} CASCADE;
-            ''')
+                DROP TABLE IF EXISTS {self.tables_names['selection']} CASCADE;
+                DROP TABLE IF EXISTS {self.tables_names['preferences']} CASCADE;
+                DROP TABLE IF EXISTS {self.tables_names['users']} CASCADE;
+                DROP TABLE IF EXISTS {self.tables_names['photos']} CASCADE;
+                ''')
             conn.commit()
-        conn.close()
+
+    def insert_request(self, tables_name: str, current_request_values: list): # заносит поисковой запрос в БД
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        INSERT INTO {self.tables_names[tables_name]} (requester, age, gender, city, country)
+                        VALUES %s
+                        RETURNING id_selection;
+                        ''', (current_request_values,))
+            id_sel = (cur.fetchone())[0]
+        return id_sel
+
+    def insert_base(self, tables_name1: str, tables_name2: str, tables_name3: str, preferences: list, id_selection: int, columns_values: list, photo_values: list): # заносит данные в БД по запросу
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        INSERT INTO {self.tables_names[tables_name1]} (favorites, black_list, viewed)
+                        VALUES %s
+                        RETURNING id_preferences;
+                        ''', (preferences,))
+            id_pref = (cur.fetchone())[0]
+
+            cur.execute(f'''
+                        INSERT INTO {self.tables_names[tables_name2]} (id_vk_user, name, surname, user_age, id_selection, id_preferences) 
+                        VALUES (%s,%s,%s,%s,%s,%s)
+                        RETURNING id_vk_user;
+                        ''', (columns_values[0], columns_values[1], columns_values[2], columns_values[3], id_selection, id_pref))
+            id_vk = (cur.fetchone())[0]
+
+            cur.execute(f'''
+                        INSERT INTO {self.tables_names[tables_name3]} (id_vk_user, link, likes)
+                        VALUES (%s, %s, %s);
+                        ''', (id_vk, photo_values[0], photo_values[1]))
+            conn.commit()
+        # conn.close()
+
+    def change_favorites(self, id_vk_user: int, favorites_status: bool): # меняет статус на избранный нужен id и ключ True
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        UPDATE preferences SET favorites=%s
+                        WHERE id_preferences=(SELECT id_preferences FROM users WHERE id_vk_user=%s);
+                        ''', (favorites_status, id_vk_user))
+            conn.commit()
+
+    def change_black_list(self, id_vk_user: int, black_list: bool): # меняет статус на черный список нужен id и ключ True
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        UPDATE preferences SET black_list=%s
+                        WHERE id_preferences=(SELECT id_preferences FROM users WHERE id_vk_user=%s);
+                        ''', (black_list, id_vk_user))
+            conn.commit()
+
+    def change_viewed(self, id_vk_user: int, viewed_status: bool): # меняет статус на просмотренный нужен id и ключ True
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        UPDATE preferences SET viewed=%s
+                        WHERE id_preferences=(SELECT id_preferences FROM users WHERE id_vk_user=%s);
+                        ''', (viewed_status, id_vk_user))
+            conn.commit()
+
+    def show_favorites(self, favorites_status: bool): # показывает избранных если ключ True (без черного списка)
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        SELECT name, surname, user_age, p.id_preferences FROM users u
+                        JOIN preferences p ON u.id_preferences = p.id_preferences
+                        WHERE favorites=%s and black_list=False;
+                        ''', (favorites_status,))
+            favorites_list = cur.fetchall()
+        return favorites_list
+
+    def show_black_list(self, black_list_status: bool): # показывает черный список если ключ True
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        SELECT name, surname, user_age, p.id_preferences FROM users u
+                        JOIN preferences p ON u.id_preferences = p.id_preferences
+                        WHERE black_list=%s;
+                        ''', (black_list_status,))
+            black_list_list = cur.fetchall()
+        return black_list_list
+
+    def show_viewed(self, viewed_status: bool): # показывает просмотренных если ключ True (без черного списка)
+        with conn.cursor() as cur:
+            cur.execute(f'''
+                        SELECT name, surname, user_age, p.id_preferences FROM users u
+                        JOIN preferences p ON u.id_preferences = p.id_preferences
+                        WHERE viewed=%s and black_list=False;
+                        ''', (viewed_status,))
+            viewed_list = cur.fetchall()
+        return viewed_list
+                        
+#
+# def user_db():
+#     test = Database()
 
 
-def user_db():
-    test = Database()
+test = Database()
+test.drop_tables()
+test.create_tables()
 
